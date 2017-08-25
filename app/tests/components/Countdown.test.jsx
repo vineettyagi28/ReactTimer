04da8334aef23f3e11e41cb0f2c2cc2c32265bec
Vineet Tyagi <Vineet.Tyagi@iontrading.com>
2017-08-25 17:04:58 +0530

    Added Countdown timer tests

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', function(){
+  it('should exist', function(){
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCoundown', () => {
+    it('should set state to started and count down', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      }, 1001);
+    });
+
+    it('should not count down to negative numbers', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 2001);
+    });
+  });
+});
