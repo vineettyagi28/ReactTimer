bcbf615dc593dbf089321c4a3ca76c8109cc880d
Vineet Tyagi <Vineet.Tyagi@iontrading.com>
2017-08-25 15:24:58 +0530

    Added clock form and its tests

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
       Timer: 'app/components/timer.jsx',
       Countdown: 'app/components/countdown.jsx',
       applicationStyles: 'app/styles/app.scss',
-      Clock: 'app/components/Clock.jsx'
+      Clock: 'app/components/Clock.jsx',
+      CountdownForm: 'app/components/CountdownForm.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
